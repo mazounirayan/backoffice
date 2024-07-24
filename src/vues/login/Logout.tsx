067228f776aff6ab8e7ee4a47a5ba3f4da391057
mazouni <mazounirayan@gmail.com>
2024-07-24 13:31:48 +0200

    version  final

--- src/vues/login/Logout.tsx
+++ src/vues/login/Logout.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {  useNavigate } from 'react-router-dom';
 
 const LogoutPage: React.FC = () => {
